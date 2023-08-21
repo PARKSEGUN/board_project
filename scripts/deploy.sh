20bc1e197d2a767a1d4acf4b3ae765531ae089e0
박세건 <90709751+PARKSEGUN@users.noreply.github.com>
2023-08-22 02:05:21 +0900

    배포 자동화 확인4

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 REPOSITORY=/home/ec2-user/app/step2
-PROJECT_NAME=springboot-webservice
+PROJECT_NAME=board_project
 
 echo "> Build 파일 복사"
 
@@ -9,8 +9,8 @@ cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(pgrep -fl springboot-webservice | grep jar | awk '{print $1}')
-#pgrep -fl springboot-webservice | grep jar -> 애플리케이션 이름으로된 jar 프로그램 찾기
+CURRENT_PID=$(pgrep -fl board_project | grep jar | awk '{print $1}')
+#pgrep -fl board_project | grep jar -> 애플리케이션 이름으로된 jar 프로그램 찾기
 #awk '{print $1}' -> 해당 ID를 찾는다.
 
 echo "현재 구동중인 어플리케이션 pid: $CURRENT_PID"
